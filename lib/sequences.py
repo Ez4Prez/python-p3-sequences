43765996b44c54c0014c5465d7cf05b79c4d2b29
Ezra Mays Richards <ezra1almond@gmail.com>
2024-10-12 22:33:09 -0400

    fibonacci excercise

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+
+    fib_sequence = [0, 1]
+
+    while len(fib_sequence) < length:
+        added_num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(added_num)
+
+    print(fib_sequence[:length])
+
+
+    
